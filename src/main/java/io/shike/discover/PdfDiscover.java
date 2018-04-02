7d6a265fa2e73a10c74f2c1956c16a67e9597b94
Ranger Tsao <cao.zhifu@gmail.com>
2018-04-02 16:42:30 +0800

    add line line separator
    Signed-off-by: Ranger Tsao <cao.zhifu@gmail.com>

--- src/main/java/io/shike/discover/PdfDiscover.java
+++ src/main/java/io/shike/discover/PdfDiscover.java
@@ -6,9 +6,10 @@ import org.apache.pdfbox.text.PDFTextStripperByArea;
 
 import java.awt.geom.Rectangle2D;
 import java.io.IOException;
+import java.util.stream.Stream;
 
 /**
- * @author Ranger Tsao(https://github.com/boliza)
+ * @author Ranger Tsao(https://github.com/boliza)z
  */
 public class PdfDiscover {
 
@@ -17,11 +18,19 @@ public class PdfDiscover {
 
     PDFTextStripperByArea stripper = new PDFTextStripperByArea();
     PDPage page = pdf.getPage(25);
+    System.out.println("page:" + page.getMediaBox().toString());
     stripper.addRegion("header", new Rectangle2D.Double(0, 0, 595.22, 60));
     stripper.addRegion("title", new Rectangle2D.Double(0, 60, 595.22, 70));
+    stripper.addRegion("content", new Rectangle2D.Double(0, 150, 595.22, 760));
+    stripper.addRegion("footer", new Rectangle2D.Double(0, 762, 595.22, 80));
     stripper.extractRegions(page);
-    System.out.println(stripper.getTextForRegion("header").trim());
-    System.out.println(stripper.getTextForRegion("title").trim());
+    System.out.println(stripper.getTextForRegion("header"));
+    System.out.println(stripper.getTextForRegion("title"));
+    System.out.println(stripper.getTextForRegion("footer"));
+    System.out.println(stripper.getTextForRegion("content"));
+
+    Stream.of(stripper.getTextForRegion("content").split(System.lineSeparator())).forEach(s -> System.out.println(s + "~~~2b"));
+
   }
 
 }
